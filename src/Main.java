07a0f2d2047b6d8dd651829a843c0b712ae71cdc
Javid <sairexm@gmail.com>
2024-05-27 11:47:51 +0400

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        // TASK 1
+        int firstTask1 = 1500000;
+        byte firstTask2 =  125;
+        short firstTask3 = 30000;
+        long firstTask4 = 99999999;
+        float firstTask5 = 3.44214f;
+        double firstTask6 = 3.14159265359;
+        System.out.println("Значение переменной firstTask1 с типом int равно " + firstTask1);
+        System.out.println("Значение переменной firstTask2 с типом byte равно " + firstTask2);
+        System.out.println("Значение переменной firstTask3 с типом short равно " + firstTask3);
+        System.out.println("Значение переменной firstTask4 с типом long равно " + firstTask4);
+        System.out.println("Значение переменной firstTask5 с типом float равно " + firstTask5);
+        System.out.println("Значение переменной firstTask6 с типом double равно " + firstTask6);
+
     }
 }
